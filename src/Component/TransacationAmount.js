a0fcaf8138ee4aeaa2970952c5848ff581529e50
Mohsin <55811423+mosin92@users.noreply.github.com>
2020-08-23 23:13:19 +0500

    commit 4
    Adding btn

--- src/Component/TransacationAmount.js
+++ src/Component/TransacationAmount.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import '../App.css';
+    import React from 'react'
+    import '../App.css';
 
-export const TransacationAmount = () => {
-    return (
-        <div className="transacation-main">
-         <div className="transacation-input"> 
-             <input id="input-1" type="text" placeholder="Enter Desciption" ></input>
+    export const TransacationAmount = () => {
+        return (
+            <div className="master">
+              <div className="transacation-main">
+                   <div className="transacation-input"> 
+                   <input id="input-1" type="text" placeholder="Enter Desciption" ></input>
+                   </div>
+                   <div className="transaction-icon">
+                  <button className="transacation-button">+</button>
+                  </div>
+                 
+                  
+                </div>
+                <div className="transacation-input-2-main">
+                  <div className="input-2">
+                    <input id="input-2-field" type="text" placeholder="Enter Amount"></input>
+                    </div>
+                    <div className="btn-2">
+                   <button className="btn-action">+</button>
+                    </div>
+                </div>
             </div>
-            <div className="transaction-icon">
-               <button className="transacation-button">+</button>
-            </div>
-    </div>
-    )
-}
+        )
+    }
